9d369ea6910f6b9b4b0213e1dbee5bd9e02b3719
jawblia <juliabell021@gmail.com>
2020-11-14 16:23:12 -0500

    refactoring checkUser route

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(jwtSecret));
 
 app.use((req, res, next) => {
-   console.log('middleware shtack');
-   console.log(req.cookies);
+   console.log('middleware');
    next();
 });
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,8 @@
 const User = require('./../models/userModel');
 const AppError = require('./../utils/AppError');
+const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
-
 
 //add catchAsync here lol 
 const signToken = id => {
@@ -54,17 +54,17 @@ exports.registerUser = async(req, res, next) => {
     }
 }
 
-exports.checkUser = async(req, res, next) => {
+exports.checkUser = catchAsync(async(req, res, next) => {
     let token;
-   
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-      ) {
-        token = req.headers.authorization.split(' ')[1];
-      } else if (req.cookies.jwt) {
+    if (req.cookies.jwt) {
         token = req.cookies.jwt;
       }    
+
+    if (!token) {
+        return next(
+          new AppError('You are not logged in! Please log in to get access.', 401)
+        );
+    }
 
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
     const currentId = decoded.id;
@@ -76,4 +76,4 @@ exports.checkUser = async(req, res, next) => {
         }
       });
 
-};
+});
