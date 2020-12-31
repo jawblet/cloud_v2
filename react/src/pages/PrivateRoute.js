83751e60c9c20ace06a95bdb1ff9fc36f11a616f
jawblia <juliabell021@gmail.com>
2020-12-31 14:13:41 -0500

    loading fix

--- react/src/pages/PrivateRoute.js
+++ react/src/pages/PrivateRoute.js
@@ -14,15 +14,13 @@ export default function PrivateRoute(props) {
         //return loading component 
         if(isLoading) {
           return <LoadingPage/>;
-        } else {
-            if(user){
-                    return ( <Route {...rest} render={(props) => (<Component {...props}/>)}/>)
-                } else {
-                    return <Redirect to='/home'/>
-                }
-        }
+        } 
 
-       
+        if(user){
+            return ( <Route {...rest} render={(props) => (<Component {...props}/>)}/>)
+        } else {
+            return <Redirect to='/home'/>
+        }
 }
 
 
