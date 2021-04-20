bb43c08c86c8e9ca095c5af88c413190d2e1d062
jawblia <juliabell021@gmail.com>
2021-04-19 22:11:40 -0400

    changed proxy

--- react/src/index.js
+++ react/src/index.js
@@ -6,11 +6,13 @@ import axios from 'axios';
   
 if(process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'https://arriveanywhere.herokuapp.com/';
+} else {
+  axios.defaults.baseURL = "http://localhost:5000";
 }
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
-
+ 
 ReactDOM.render(
   <React.StrictMode>
     <App />
