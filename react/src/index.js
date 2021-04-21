b3758dda5c8898483d6569dfe575c86cde4237dd
jawblia <juliabell021@gmail.com>
2021-04-21 13:15:01 -0400

    fix routing

--- react/src/index.js
+++ react/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
   
-
+/*
 if(process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'https://arriveanywhere.xyz';
 } 
+*/
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
